f8977c8b7f4b36c1eeeb184b80b6ebd5dff4ebef
Ladislav Prskavec <ladislav@prskavec.net>
2013-07-17 17:41:03 +0200

    Refactor step 5 Add html_statement

--- Customer.php
+++ Customer.php
@@ -19,18 +19,31 @@ class Customer
 
 	public function statement()
 	{
-		$total_amount = 0;
 		$result = "Rental Record for $this->name\n";
 		foreach ($this->rentals as $element) {						
       // show figures for this rental
       $result .= "\t" . $element->movie->title . "\t" . (string) $element->charge() . "\n";
-      $total_amount += $element->charge();
 		}
  		// add footer lines
     $result .= "Amount owed is {$this->total_charge()} \n";
     $result .= "You earned {$this->total_frequent_renter_points()} frequent renter points";
     return $result;
 	}	
+
+ 	public function html_statement()
+ 	{
+    $result = "<h1>Rentals for <em>#{@name}</em></h1><p>\n";
+    foreach ($this->rentals as $element) {						
+      // show figures for this rental
+      $result .= "\t" + $element->movie->title . ": " . (string) $element->charge() . "<br>\n";
+    }
+    // add footer lines
+    $result .= "<p>You owe <em>{$this->total_charge()}</em><p>\n";
+    $result .= "On this rental you earned " .
+           "<em>{$this->total_frequent_renter_points()}</em> " .
+           "frequent renter points<p>";
+    return $result;
+	}
 
 	public function total_charge()
 	{
