2dcf8b2ef73193c85e004eceafa78053282c9fe3
Ladislav Prskavec <ladislav@prskavec.net>
2013-07-17 17:37:19 +0200

    Refactor step 4 Removing Temps

--- Customer.php
+++ Customer.php
@@ -20,26 +20,33 @@ class Customer
 	public function statement()
 	{
 		$total_amount = 0;
-		$frequent_renter_points = 0;
 		$result = "Rental Record for $this->name\n";
 		foreach ($this->rentals as $element) {						
-			$frequent_renter_points += $element->frequent_renter_points();
       // show figures for this rental
       $result .= "\t" . $element->movie->title . "\t" . (string) $element->charge() . "\n";
       $total_amount += $element->charge();
 		}
  		// add footer lines
     $result .= "Amount owed is {$this->total_charge()} \n";
-    $result .= "You earned $frequent_renter_points frequent renter points";
+    $result .= "You earned {$this->total_frequent_renter_points()} frequent renter points";
     return $result;
 	}	
 
 	public function total_charge()
 	{
-			$result = 0;
-			foreach ($this->rentals as $element) {						
-				$result += $element->charge();
-			}
-			return $result;
+		$result = 0;
+		foreach ($this->rentals as $element) {						
+			$result += $element->charge();
+		}
+		return $result;
+	}
+
+	public function total_frequent_renter_points()
+	{
+		$result = 0;
+		foreach ($this->rentals as $element) {						
+			$result += $element->frequent_renter_points();
+		}
+		return $result;		
 	}
 }
