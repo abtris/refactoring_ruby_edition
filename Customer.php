870e5819db9dae15814492d7127f3c6a9c5d5891
Ladislav Prskavec <ladislav@prskavec.net>
2013-07-17 16:51:27 +0200

    Refactor step 1 extract method from statement

--- Customer.php
+++ Customer.php
@@ -23,6 +23,25 @@ class Customer
 		$frequent_renter_points = 0;
 		$result = "Rental Record for $this->name\n";
 		foreach ($this->rentals as $element) {			
+			$this_amount = $this->amount_for($element);
+			// add frequent renter points
+      $frequent_renter_points += 1;
+      // add bonus for a two day new release rental
+      if ($element->movie->price_code == Movie::NEW_RELEASE && $element->days_rented > 1) {
+          $frequent_renter_points += 1;
+      }
+      // show figures for this rental
+      $result .= "\t" . $element->movie->title . "\t" . (string) $this_amount . "\n";
+      $total_amount += $this_amount;
+		}
+ 		// add footer lines
+    $result .= "Amount owed is $total_amount\n";
+    $result .= "You earned $frequent_renter_points frequent renter points";
+    return $result;
+	}
+
+	public function amount_for($element)
+	{
 			$this_amount = 0;
 			// determine amounts for each line
 			switch ($element->movie->price_code) {
@@ -43,19 +62,6 @@ class Customer
 						$this_amount += ($element->days_rented - 3) * 1.5;
 					}	
 			}		
-			// add frequent renter points
-      $frequent_renter_points += 1;
-      // add bonus for a two day new release rental
-      if ($element->movie->price_code == Movie::NEW_RELEASE && $element->days_rented > 1) {
-          $frequent_renter_points += 1;
-      }
-      // show figures for this rental
-      $result .= "\t" . $element->movie->title . "\t" . (string) $this_amount . "\n";
-      $total_amount += $this_amount;
-		}
- 		// add footer lines
-    $result .= "Amount owed is $total_amount\n";
-    $result .= "You earned $frequent_renter_points frequent renter points";
-    return $result;
+			return $this_amount;
 	}
 }
